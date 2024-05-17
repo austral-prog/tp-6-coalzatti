e88eb4db9302f67719b22af36b457696825184e0
Constanza Sofía Alzatti <162724190+coalzatti@users.noreply.github.com>
2024-05-17 06:35:52 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,70 @@
-# Replace the "ANSWER HERE" with your answer
-
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(list_to_remove_elements)>=6:
+        del list_to_remove_elements[5]
+        del list_to_remove_elements[4]
+        del list_to_remove_elements[0]
+        return list_to_remove_elements
+        
+    elif len(list_to_remove_elements)>=5:
+        del list_to_remove_elements[5]
+        del list_to_remove_elements[0]
+        return list_to_remove_elements
+        
+    elif len(list_to_remove_elements)>0 and list_to_remove_elements<=4:
+        del list_to_remove_elements[0]
+        return list_to_remove_elements
+list1=['Red', 'Green', 'White', 'Black', 'Pink','yellow']
+
 
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    list2=['Red', 'Green', 'White', 'Black']
+    list_to_add_elements.insert(0,'Pink')
+    list_to_add_elements.append('Yellow')
+    return list_to_add_elements
 
+
+    
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
+    lista3=[]
+    if list_to_check==[]:
+        return "La lista esta vacia"
+    else:
+        return "La lista no esta vacia"
+ 
+
 
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
+    list4=['Black', 'Pink', 'Yellow', 'Red', 'Green', 'White']
+    list5=['Red', 'Green', 'Yellow', 'White', 'Black', 'Pink']
+    if len(list_to_compare1)>=3 and len(list_to_compare2)>=3:
+        if list_to_compare1[2]==list_to_compare2[2]:
+            return 'True'
+        else:
+            return 'False'
+    elif (len(list_to_compare1)>0 and len(list_to_compare1)<3) or (len(list_to_compare2)>0 and len(list_to_compare2)<3):
+        return 'False'
+    else:
+        return 'False'
+
+
 
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    list6=[[1, 2, 3], [4, 5, 6, 7, 8], [9, 10, 11, 12]]
+    if len(list_of_lists_to_modify)>2:
+        if len(list_of_lists_to_modify[0])>=1:
+            el1=list_of_lists_to_modify[0][0:2]
+        else:
+            el1=[]
+    if len(list_of_lists_to_modify[1])>=2:
+        el2=list_of_lists_to_modify[1][1:4]
+    else:
+        el2=[]
+    if len(list_of_lists_to_modify[2])>=1:
+            el3=list_of_lists_to_modify[2][-2:]
+    else:
+        el3=[]
+        return [el1,el2,el3]
